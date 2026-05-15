21da73877f021fef1bbf28cba3872faf75c9c12e
İBRAHİM YILMAZ <ibrahimylmazirahi@hotmail.com>
2026-05-15 11:50:42 +0300

    initial commit

---
+++ src/main/java/com/example/devops41/Devops41Application.java
@@ -0,0 +1,13 @@
+package com.example.devops41;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops41Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops41Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devops41/Devops41ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops41ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
